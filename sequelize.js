0b61d7b41da145e5413cce426e5df41b612bbf90
Jessica Swenson <jessicamswens@gmail.com>
2022-10-08 17:19:10 -0600

    data base

--- sequelize.js
+++ sequelize.js
@@ -1,10 +1,10 @@
 // require('dotenv').config();
-// const {DATABASE_URL} = process.env;
+const { DATABASE_URL } = process.env;
 
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    "postgresql://jswens:v2_3ug9n_Y2VN2f7pnGfU5tKW8uf4Lgi@db.bit.io/jswens/test",
+    DATABASE_URL,
     {
         dialect: 'postgres',
         dialectOptions: {
